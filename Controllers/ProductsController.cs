0a88ef33fcde7e31a06ec14933d562d39eb38a9f
Abisagy Nafula <abisakyn@gmail.com>
2024-05-04 18:34:04 +0300

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "Ok";
+        }
+    }
+}
